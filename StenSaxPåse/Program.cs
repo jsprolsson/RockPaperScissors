ad78779cef96e7e9dc2a2b9eb0e3eba2c0513100
jsprolsson <jesper.p.olsson@hotmail.com>
2021-09-16 15:05:53 +0200

    moved game to own class, cleaned up main

--- StenSaxPåse/Program.cs
+++ StenSaxPåse/Program.cs
@@ -6,94 +6,103 @@ namespace StenSaxPåse
     {
         static void Main(string[] args)
         {
-            int computerScore = 0;
-            int userScore = 0;
-            string userChoice;
 
-            Random random = new Random();
+            RockPaperScissors.Play();
 
-            int randomNumber = random.Next(1, 3);
+        }
 
-            //Console.Write("STEN, SAX eller PÅSE: ");
-            //userChoice = Console.ReadLine();
-            //string userChoiceUpper = userChoice.ToUpper();
+        class RockPaperScissors
+        {
 
-            do
+            public static void Play()
             {
-                Console.WriteLine($"Computer score: {computerScore}");
-                Console.WriteLine($"Player score: {userScore}");
-                Console.WriteLine("--------------------------------------");
-                Console.Write("STEN, SAX eller PÅSE: ");
-                userChoice = Console.ReadLine();
-                string userChoiceUpper = userChoice.ToUpper();
+                Random random = new Random();
+                int computerScore = 0;
+                int userScore = 0;
+                string userChoice;
+                int randomNumber = random.Next(1, 3);
 
-                if (randomNumber == 1)
+                Console.WriteLine("Best of 1, 3, 5 or 7? ");
+                int bestOfNumber = int.Parse(Console.ReadLine());
+
+                do
                 {
-                    Console.WriteLine("Computer plays STEN.");
+                    Console.WriteLine($"Computer score: {computerScore}");
+                    Console.WriteLine($"Player score: {userScore}");
+                    Console.WriteLine("--------------------------------------");
+                    Console.Write("STEN, SAX eller PÅSE: ");
+                    userChoice = Console.ReadLine();
+                    string userChoiceUpper = userChoice.ToUpper();
 
-                    if (userChoiceUpper == "STEN")
+                    if (randomNumber == 1)
                     {
-                        Console.WriteLine("It's a tie!\n No points given.");
+                        Console.WriteLine("Computer plays STEN.");
+
+                        if (userChoiceUpper == "STEN")
+                        {
+                            Console.WriteLine("It's a tie!\n No points given.");
+                        }
+                        else if (userChoiceUpper == "SAX")
+                        {
+                            Console.WriteLine("Player lose!");
+                            computerScore++;
+                        }
+                        else if (userChoiceUpper == "PÅSE")
+                        {
+                            Console.WriteLine("Player win!");
+                            userScore++;
+                        }
+
                     }
-                    else if (userChoiceUpper == "SAX")
+                    else if (randomNumber == 2)
                     {
-                        Console.WriteLine("Player lose!");
-                        computerScore++;
+
+                        Console.WriteLine("Computer plays SAX.");
+
+                        if (userChoiceUpper == "STEN")
+                        {
+                            Console.WriteLine("Player wins!");
+                            userScore++;
+                        }
+                        else if (userChoiceUpper == "SAX")
+                        {
+                            Console.WriteLine("It's a tie!\nNo point given.");
+                        }
+                        else if (userChoiceUpper == "PÅSE")
+                        {
+                            Console.WriteLine("Computer win!");
+                            computerScore++;
+                        }
+
                     }
-                    else if (userChoiceUpper == "PÅSE")
+                    else if (randomNumber == 3)
                     {
-                        Console.WriteLine("Player win!");
-                        userScore++;
+                        Console.WriteLine("Computer plays PÅSE.");
+
+                        if (userChoiceUpper == "STEN")
+                        {
+                            Console.WriteLine("Computer win!");
+                            computerScore++;
+                        }
+                        else if (userChoiceUpper == "SAX")
+                        {
+                            Console.WriteLine("Player win!");
+                            userScore++;
+                        }
+                        else if (userChoiceUpper == "PÅSE")
+                        {
+                            Console.WriteLine("It's a tie!\n No points given.");
+                        }
+
                     }
 
-                }
-                else if (randomNumber == 2)
-                {
-
-                    Console.WriteLine("Computer plays SAX.");
-
-                    if (userChoiceUpper == "STEN")
-                    {
-                        Console.WriteLine("Player wins!");
-                        userScore++;
-                    }
-                    else if (userChoiceUpper == "SAX")
-                    {
-                        Console.WriteLine("It's a tie!\nNo point given.");
-                    }
-                    else if (userChoiceUpper == "PÅSE")
-                    {
-                        Console.WriteLine("Computer win!");
-                        computerScore++;
-                    }
-
-                }
-                else if (randomNumber == 3)
-                {
-                    Console.WriteLine("Computer plays PÅSE.");
-
-                    if (userChoiceUpper == "STEN")
-                    {
-                        Console.WriteLine("Computer win!");
-                        computerScore++;
-                    }
-                    else if (userChoiceUpper == "SAX")
-                    {
-                        Console.WriteLine("Player win!");
-                        userScore++;
-                    }
-                    else if (userChoiceUpper == "PÅSE")
-                    {
-                        Console.WriteLine("It's a tie!\n No points given.");
-                    }
-
-                }
-
-                Console.ReadKey();
-                Console.Clear();
+                    Console.ReadKey();
+                    Console.Clear();
 
 
-            } while (computerScore != 3 && userScore != 3);
+                } while (computerScore != bestOfNumber && userScore != bestOfNumber);
+            }
+
 
 
 
